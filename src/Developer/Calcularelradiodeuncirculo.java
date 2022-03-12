963dddeae86a8e853b7ac971e1601625ba688abd
carlos-arman <minerocarlos28@gmail.com>
2022-03-11 16:19:50 -0800

    Programa

--- src/Developer/Calcularelradiodeuncirculo.java
+++ src/Developer/Calcularelradiodeuncirculo.java
@@ -1,24 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package Developer;
 
-import java.text.DecimalFormat:
-import java.text.NumbreFormat;
+import java.text.DecimalFormat;
+
+
 /**
  *
  * @author ITCA
  */
 public class Calcularelradiodeuncirculo {
     public static void main(String[] args) {
-        NumbreFormat formato = new DecimalFormat("#0.00");
+        DecimalFormat formato = new DecimalFormat("#0.00");
         double area, radio;
         final double pi = 3.1416;
         
         area = 145;
         radio = Math.sqrt(area/pi);
-        System.out.println("Radio: " + radio + "cm");
+        System.out.println("Radio: " + formato.format (radio) + "cm");
     }
 }
