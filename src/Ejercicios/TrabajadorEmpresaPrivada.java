89bf829d4f29197ffbb44eecf301a79bfbe287d7
carlos-arman <minerocarlos28@gmail.com>
2022-03-14 22:05:14 -0700

    Empresa Privada

--- src/Ejercicios/TrabajadorEmpresaPrivada.java
+++ src/Ejercicios/TrabajadorEmpresaPrivada.java
@@ -5,14 +5,21 @@
 package Ejercicios;
 
 /**
- Si un trabajador de la empresa privada está afiliado a las AFP y los porcentajes 
- * que se paga a las AFP es de 6% por parte del trabajador y 6.5% por parte del 
- * empleador, si el empleado gana $500 al mes,¿Cuánto está pagando a la AFP el 
- * trabajador en concepto de empleado y de empleador?
+  Si un trabajador de la empresa privada está afiliado a las AFP y los porcentajes 
+  que se paga a las AFP es de 6% por parte del trabajador y 6.5% por parte del 
+  empleador, si el empleado gana $500 al mes,¿Cuánto está pagando a la AFP el 
+  trabajador en concepto de empleado y de empleador?
  */
 public class TrabajadorEmpresaPrivada {
     public static void main(String[] args){
         double AFPTrabajador, AFPEmpleador, Trabajador, Empleado;
+        
+        AFPTrabajador = 0.06;
+        AFPEmpleador = 0.065;
+        Empleado = 500;
+        
+        
+        
         
         
     }
